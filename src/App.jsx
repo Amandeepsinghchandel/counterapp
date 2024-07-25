b636e70e4fdc15ffa34071f8ba8c25f0b34ce15a
Aman <121555482+Amandeepsinghchandel@users.noreply.github.com>
2024-07-25 22:16:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import CounterApp from './component/CounterApp';
+
+
+function App() {
+
+  return (
+    <div>
+       <CounterApp/>
+    </div>
+  );
+}
+
+export default App;
